952e0287fdddc1669c3ed30b6609aeb348d97258
Piyush Paliwal <piyushpaliwalofficial868@gmail.com>
2025-03-06 11:30:14 +0530

    onestop

--- frontend/src/components/home/HelpBussiness.tsx
+++ frontend/src/components/home/HelpBussiness.tsx
@@ -27,11 +27,10 @@ function HelpBussiness({heading,cardscontent}:props) {
 
     
   return (
-    <div className="flex flex-col  md:py-[60px] gap-5 bg-background-white-100 w-full  items-center justify-center py-9 ">
-      <div className=" w-full flex flex-col gap-[30px]">
-        <span className="text-[28px]/[32.2px] md:text-[43.88px]/[56px] font-normal md:px-[125px] text-center px-8  ">
+    <div className="flex flex-col py-[40px] md:py-[60px] gap-5   bg-background-white-100 w-full  items-center justify-center ">
+      <div className=" w-full flex flex-col gap-[30px] items-center">
+        <div className="text-start text-mobilesubheading/[32px] md:text-heading/[68.99px]   font-normal px-[30px]  lg:px-[125px] ">
           Let’s see how we can help your business
-          </span>
         </div>
         <Marquee speed={50}>
           {" "}
@@ -59,7 +58,7 @@ function HelpBussiness({heading,cardscontent}:props) {
           </div>
         </Marquee>
       </div>
-   
+    </div>
   );
 }
 
---
+++ frontend/src/components/home/One-stop.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+
+const Onestopcard = ({ icon, heading, subheading,color }:any) => {
+  return (
+    <div
+      className="flex flex-col gap-[30px] min-w-[271px] min-h-[671px] max-h-min max-w-min rounded-cardradius p-[30px]"
+      style={{ backgroundColor: color }}
+    >
+      <div className="grid grid-rows-[8fr_7fr] h-full">
+        <div className="flex flex-col gap-[30px]">
+          <div className="flex justify-start">
+            <Image src={icon} width={40} height={40} alt="icon" />
+          </div>
+          <div className="text-avg font-semibold">{heading}</div>
+        </div>
+        <div className="text-body">{subheading}</div>
+      </div>
+    </div>
+  );};
+
+const Onestop = () => {
+
+    const cardata = [
+      {
+        color: "#FFE599",
+        icon: "/images/onestop/one.png",
+        heading: "Creative  + Technical Solutions that examine top quality ✨",
+        subheading:
+          "Welcome to Designera. Quickly submit a brief, review in platform, keep track of usage and more in one easy place.",
+      },
+      {
+        color: "#FFCCE2",
+        icon: "/images/onestop/two.png",
+        heading: "Marketing + Media Solutions that drives revenue 💵",
+        subheading:
+          "Grow your organic presence and reduce dependence on paid channels with optimised content",
+      },
+      {
+        color: "#BCF5D7",
+        icon: "/images/onestop/three.png",
+        heading: "Hire the Top 1% of Global Creative Talent 🤩",
+        subheading:
+          "From website to social media, we have you covered with our expert team of designers and developers.",
+      },
+      {
+        color: "#D4BBF6",
+        icon: "/images/onestop/four.png",
+        heading: "Find the right opportunity around the table 💼",
+        subheading:
+          "Our team of strategists and content creators work together to create content that resonates with your audience.",
+      },
+    ];
+   return <section className="expertise md:py-[60px] py-[40px] md:px-[125px] px-[40px] flex flex-col gap-[30px] justify-center items-center w-full  ">
+     <div className="flex flex-col gap-[30px] w-full">
+       <div className="flex text-mediumheading">
+         A One-Stop Solution Crafted to Elevate Your Brand
+       </div>
+       <div className="flex justify-between">
+        {cardata.map((item, index) => (
+            <Onestopcard {...item}/>
+        ))}
+       </div>
+     </div>
+   </section>;
+}
+
+export default Onestop;
